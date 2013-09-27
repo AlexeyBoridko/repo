9f4136f4cbe04d1bfb91461a4d8c51a27300aca4
AlexeyBoridko <oleksiyua@gmail.com>
2013-09-27 16:47:32 +0300

    code refactoring

--- prj_testDjangoApp/models.py
+++ prj_testDjangoApp/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question
 
@@ -14,8 +15,10 @@ class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
     votes = models.IntegerField()
+
     def __str__(self):
         return self.choice
+
 
 class Test(models.Model):
     field1 = models.NullBooleanField()
